721f4f9a7ca9a3dc29aa2364659270eee63676a8
Vedadnt <vedantshuklaji15@gmail.com>
2025-12-10 21:31:39 +0530

    Add missing sanity schema and structure files

---
+++ sanity/schemaTypes.ts
@@ -0,0 +1,17 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+// We assume your actual schema files are inside a 'schemas' folder
+// If your folder is named differently, adjust these imports
+import post from './schemaTypes/post'
+// import category from './schemas/category'
+// import author from './schemas/author' // Uncomment if you have this
+// import blockContent from './schemas/blockContent' // Uncomment if you have this
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    post,
+    category,
+    // author,
+    // blockContent,
+  ],
+}
