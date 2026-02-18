88a9720a664c364f4fe75911b670999447c86bd6
Vedadnt <vedantshuklaji15@gmail.com>
2026-02-18 21:30:27 +0530

    update 2

--- app/web-stories/[id]/page.tsx
+++ app/web-stories/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { client, urlFor } from "../sanity/lib/sanity";
+import { client, urlFor } from "@/sanity/lib/sanity"; 
 import Image from "next/image";
 import Link from "next/link";
 import { FaTimes, FaArrowRight, FaArrowLeft } from "react-icons/fa";
