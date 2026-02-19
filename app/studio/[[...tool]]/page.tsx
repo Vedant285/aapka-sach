f5bb7e4b4c9e08f023e99d8667365b5637a23b1a
Vedadnt <vedantshuklaji15@gmail.com>
2026-02-19 19:33:29 +0530

    Fix: Resolved NextStudio type error and unified config import

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,6 +1,6 @@
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import config from '@/sanity/sanity.config'
 
 export const dynamic = 'force-static'
 
