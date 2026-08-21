1dda11746561921d85c8e97d649f536c755bc401
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2026-08-21 12:01:07 +0530

    Update structure.ts

--- sanityStudio/structure.ts
+++ sanityStudio/structure.ts
@@ -2,7 +2,7 @@ import type { StructureBuilder } from 'sanity/structure'
 
 export const structure = (S: StructureBuilder) =>
   S.list()
-    .title('Aapka Sach Content')
+    .title('Aaj Ka Sach Content')
     .items([
       ...S.documentTypeListItems(),
     ])
