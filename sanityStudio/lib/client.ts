dda66319bfa30b6c704a0735e6acc074b29de510
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2026-08-23 13:34:17 +0530

    Update client.ts

--- sanityStudio/lib/client.ts
+++ sanityStudio/lib/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'next-sanity'
-import imageUrlBuilder from '@sanity/image-url' // You may need to run 'npm install @sanity/image-url'
+import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
   projectId: 'g1o8uwxq', 
